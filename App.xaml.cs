27962dbdd8c62f70a1fb6531248296a013fa3da3
Krisbor3 <krisborr@gmail.com>
2024-04-05 18:13:04 +0300

    extracted the api key in config file

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,9 +13,7 @@ namespace SearchForAddresses
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            // Note: it is not best practice to store API keys in source code.
-            // The API key is referenced here for the convenience of this tutorial.
-            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPK5177dbae692f4da6b57951522b63710afAVso_ojlGfIMYaFsKn89oKKlmZlfTfLbSDZWaIwkS7r2-lLqB5BUz9W6SG07noM";
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = ConfigurationManager.AppSettings.Get("Api_Key");
         }
 
     }
